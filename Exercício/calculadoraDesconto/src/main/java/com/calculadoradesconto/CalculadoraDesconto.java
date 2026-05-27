7cd101639b511a93742af46dd0de3f7c16e89a5d
Lucas Gabriel <lucas.abade2104@gmail.com>
2026-05-27 20:10:45 -0300

    Add files via upload

---
+++ Exercício/calculadoraDesconto/src/main/java/com/calculadoradesconto/CalculadoraDesconto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.calculadoradesconto;
+
+/**
+ *
+ * @author 42420206
+ */
+public class CalculadoraDesconto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Exercício/calculadoraDesconto/src/test/java/com/calculadoradesconto/CalculadoraDescontoTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.calculadoradesconto;
+
+/**
+ *
+ * @author 42420206
+ */
+public class CalculadoraDescontoTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
